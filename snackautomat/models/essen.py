15b9b5f0a3138e177182ae0230dc6974ef90b55a
Tim K. <klein_tim@t-online.de>
2026-07-20 12:13:45 +0200

    add: preise hinzugefügt

--- snackautomat/models/essen.py
+++ snackautomat/models/essen.py
@@ -3,15 +3,17 @@
 class Essen:
     """Basis-Klasse für essbare Artikel im Snackautomaten."""
 
-    def __init__(self, verpackung, menge_gramm):
+    def __init__(self, verpackung, menge_gramm, preis):
         """Initialisiert ein Lebensmittel mit Verpackung und Gewicht.
 
         Args:
             verpackung (str): Bezeichnung der Verpackung oder des Produkts.
             menge_gramm (int|float): Gewicht des Produkts in Gramm.
+            preis (int|float): Preis des Produkts in Euro
         """
         self.verpackung = verpackung
         self.menge_gramm = menge_gramm
+        self.preis = preis
 
 class Wasabinuesse(Essen):
     """Wasabinüsse als Snack."""
@@ -25,6 +27,6 @@ class Mars(Essen):
     """Mars-Riegel als Snack."""
     pass
 
-wasabinuesse = Wasabinuesse(menge_gramm=50, verpackung="Wasabinuesse")
-algenchips = Algenchips(menge_gramm=30, verpackung="Algenchips")
-mars = Mars(menge_gramm=45, verpackung="Mars")
+wasabinuesse = Wasabinuesse(menge_gramm=50, verpackung="Wasabinuesse", preis=1.50 )
+algenchips = Algenchips(menge_gramm=30, verpackung="Algenchips", preis=1.80)
+mars = Mars(menge_gramm=45, verpackung="Mars", preis=1.20)
