1fbb490ce65849538657df98bcbb288f60ba8367
Tim K. <klein_tim@t-online.de>
2026-07-21 22:02:32 +0200

    add: class uni_netzwerk

--- snackautomat/services/uni_netzwerk.py
+++ snackautomat/services/uni_netzwerk.py
@@ -1,2 +1,15 @@
 class Uni_Netzwerk:
-    pass
+    def __init__(self, bestand: dict, kontostand: int, anzahl_snackautomaten: int):
+        self.bestand: dict = bestand
+        self.kontostand: int = kontostand
+        self.anzahl_snackautomaten: int = anzahl_snackautomaten
+
+    def bestandsdaten_empfangen(self, automat: str, produkte: dict):
+        pass
+
+    def wechselgeldbestand_empfangen(self, automat: str, bestand: int):        
+        pass
+    
+automat_1 = Uni_Netzwerk(bestand={'Cola': 20}, kontostand=100, anzahl_snackautomaten=1)
+print(automat_1.anzahl_snackautomaten)
+print(automat_1.bestand)
