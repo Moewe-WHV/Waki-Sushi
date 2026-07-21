a79579958d915e27de0020a1fb95e5d63feb9035
Tim K. <klein_tim@t-online.de>
2026-07-21 21:42:00 +0200

    fix: class Kunde - nach neuen Diagramm

--- snackautomat/models/kunde.py
+++ snackautomat/models/kunde.py
@@ -1,32 +1,50 @@
-"""Kunde-Modell für den Snackautomaten.
+from services.bank import Bank
+from models.essen import mars
 
-Dieses Modul enthält die Kundenklasse für die Interaktion mit dem Automaten.
-"""
 
 class Kunde:
-    """Modelliert einen Kunden am Snackautomaten.
-
-    Ein Kunde kann eine Produktauswahl treffen, zwischen Karte und Bar wählen,
-    eine PIN eingeben und die Ware sowie Wechselgeld entnehmen.
-    """
     def __init__(self, kontostand: int, bargeld: int, nutzt_karte: bool):
         self.kontostand: int = kontostand
-        self.bargeld: int = bargeld 
+        self.bargeld: int = bargeld
         self.nutzt_karte: bool = nutzt_karte
-        
-    def auswahl_produkt(self) -> str: 
-        pass 
-    
-    def karte_eingeben(self) -> str: 
-        return "..."
-    
-    def karte_ausgeben(self) -> str: 
-        return "..."
-    
-    def bezahlen(self, mit_karte: bool) -> str:
+        self.produkt_auswahl: str = ""
+
+    def auswahl_produkt(self, auswahl: str):
+        self.produkt_auswahl = auswahl
+
+    def karte_eingeben(self) -> str:
+        if self.nutzt_karte:
+            return "Karte eingelesen"
+        else:
+            return "Keine Karte vorhanden"
+
+    def karte_ausgeben(self) -> str:
+        return "Karte wird ausgegeben"
+
+    def bezahlen(self, mit_karte: bool, betrag: int, bank: Bank) -> str:
         if mit_karte:
-            return "..."
-        else: 
-            return "..."
-    
-    
+            autorisiert = bank.zahlung_autorisieren(betrag)
+            if autorisiert:
+                bank.kontostand_aktualisieren(betrag)
+                return "Zahlung mit Karte erfolgreich"
+            else:
+                return "Zahlung mit Karte fehlgeschlagen"
+        else:
+            if self.bargeld >= betrag:
+                self.bargeld = self.bargeld - betrag
+                return "Zahlung mit Bargeld erfolgreich"
+            else:
+                return "Zahlung mit Bargeld fehlgeschlagen"
+
+    def ware_entnehmen(self) -> str:
+        return f"{self.produkt_auswahl} wird entnommen"
+
+    def wechselgeld_entnehmen(self) -> int:
+        return 0
+
+
+kunde = Kunde(kontostand=50, bargeld=10, nutzt_karte=True)
+bank = Bank(kontostand_kunde=50, karte_gueltigkeit=True)
+
+kunde.auswahl_produkt("Wasabinuesse")
+print(kunde.bezahlen(True, mars.preis, bank))
